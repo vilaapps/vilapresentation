45cfc52b0f3ad70fc9398eeb2e2105f1ff294ec5
GuilhermeTrivilin <guilherme.pimentel@flexait.com.br>
2021-02-09 21:06:11 -0300

    adicionando listener para atualizazr o tamanho do window

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 import styled from 'styled-components';
@@ -6,21 +6,34 @@ import styled from 'styled-components';
 import { Page } from 'react-pdf';
 import { Document } from 'react-pdf/dist/esm/entry.webpack';
 
-const { innerWidth: width, innerHeight: height } = window
-
 const PdfViewer = ({ pdfBase64 }) => {
 
     const listenersKeys = ['left', 'right']
 
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+    const [width, setWidth] = useState(() => window.innerWidth)
+    const [height, setHeight] = useState(() => window.innerHeight)
+
+    useEffect(() => {
+        window.addEventListener("resize", updateWindowDimensions);
+        return window.removeEventListener("resize", updateWindowDimensions);
+    }, [])
+
+    const updateWindowDimensions = () => {
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+    }
 
     const handleNumPages = ({ numPages }) => setNumPages(numPages)
     const backPage = () => pageNumber > 1 && setPageNumber(pageNumber - 1)
     const nextPage = () => pageNumber < numPages && setPageNumber(pageNumber + 1)
     const handleKey = (key) => key === "left" ? backPage() : nextPage()
 
-    return <Wrapper>
+    return <Wrapper
+        width={width}
+        height={height}
+    >
         <Document
             file={pdfBase64}
             onLoadSuccess={handleNumPages}
@@ -40,8 +53,8 @@ const PdfViewer = ({ pdfBase64 }) => {
 }
 
 const Wrapper = styled.div`
-  width: ${width};
-  height: ${height};
+  width: ${props => props.width};
+  height: ${props => props.height};
   position: absolute;
 `
 
